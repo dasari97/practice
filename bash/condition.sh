2627e78b0b3c8b852f1052626fec9fd0175779d2
dasari97 <dasarisaikrishna97@gmail.com>
2021-12-13 17:46:48 +0530

    .

--- bash/condition.sh
+++ bash/condition.sh
@@ -3,7 +3,7 @@ echo "Enter any number"
 read num
 if [ $num -ge 10 ];
     then
-        echo "[$num..1]"
+        echo "{$num..1}"
     else [ $num -le 10 ];
         echo "Number is greater then 10"
 fi
