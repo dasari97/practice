fba9c1a50f910b88f5523943e31d070e8736a57a
dasari97 <dasarisaikrishna97@gmail.com>
2021-12-16 19:22:10 +0530

    .

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,14 +29,23 @@ resource "aws_security_group" "my-sg" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    self             = false
+    security_groups  = []
   }
 
     ingress {
     description      = "ssh"
     from_port        = 22
     to_port          = 22
-    protocol         = "ssh"
-    cidr_blocks     = ["0.0.0.0/0"] 
+    protocol         = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    self             = false
+    security_groups  = []
   }
 
   egress {
