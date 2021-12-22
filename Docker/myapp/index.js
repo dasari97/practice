021fdd789b331055a00a77a881638b91af4b8f63
dasari97 <dasarisaikrishna97@gmail.com>
2021-12-22 11:07:14 +0530

    .

--- Docker/myapp/index.js
+++ Docker/myapp/index.js
@@ -2,13 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.json([
-{
+app.get('/', (req, res) => res.json([{
 	name: 'Dasari',
-
 	email: 'dasarisaikrishna97@gmail.com'
-}
-]))
+}]))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
