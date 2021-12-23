6c3aaeb8a71ee018ce4aa29ad8003ccf6482b92d
dasari97 <dasarisaikrishna97@gmail.com>
2021-12-23 17:57:19 +0530

    .

--- Docker/myapp/index.js
+++ Docker/myapp/index.js
@@ -5,15 +5,15 @@ const port = 3000
 app.get('/', (req, res) => res.json([
 	{
 		name: "Dasari",
-		email: "dasarisaikrishna97@gmail.com",
+		email: "dasarisaikrishna97@gmail.com"
 },
 {
 	name: "sai",
-	email: "saitarak1997@gmail.com",
+	email: "saitarak1997@gmail.com"
 },
 {
 	name: "krishna",
-	email: "saikrishna9494342038@gmail.com",
+	email: "saikrishna9494342038@gmail.com"
 }
 ]))
 
